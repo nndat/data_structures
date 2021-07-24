9df9a1f3046bea2e6fbb393446d97e011bcc8b9f
nndat <nguyenngocdat90@gmail.com>
2021-07-24 18:50:22 +0700

    implementation single linked list

--- single_linked_list/sgllist.py
+++ single_linked_list/sgllist.py
@@ -13,63 +13,134 @@ class SingleLinkedList(object):
         self.begin = None
         self.end = None
 
+    def __str__(self):
+        current_node = self.begin
+        values = []
+        while current_node:
+            values.append(current_node.value)
+            current_node = current_node.next
+        return '->'.join(values)
+
     def push(self, obj):
         """
         Appends a new value on the end of list.
         """
-        pass
+        new_node = SingleLinkedListNode(obj, None)
+        if self.begin is None:
+            self.begin = new_node
+
+        if self.end is None:
+            self.end = new_node
+        else:
+            self.end.next = new_node
+            self.end = new_node
 
     def pop(self):
         """
         Removes the last item and return it.
         """
-        pass
+        last_node = self.end
 
-    def shift(self):
+        if self.begin is self.end:
+            self.begin = self.end = None
+        else:
+            current_node = self.begin
+            while current_node.next is not self.end:
+                current_node = current_node.next
+
+            self.end = current_node
+            self.end.next = None
+
+        return last_node and last_node.value or None
+
+    def shift(self, obj):
         """
         Another name for push.
         """
-        pass
+        new_node = SingleLinkedListNode(obj, None)
+        if self.begin is None:
+            self.begin = self.end = new_node
+        else:
+            new_node.next = self.begin
+            self.begin = new_node
 
     def unshift(self):
         """
         Removes the first item and return it.
         """
-        pass
+        first_node = self.begin
+        if self.begin is None:
+            return None
+        elif self.begin is self.end:
+            self.begin = self.end = None
+        else:
+            self.begin = self.begin.next
+        return first_node.value
 
     def remove(self, obj):
         """
         Finds a matching item and removes it from list.
         """
-        pass
+        current_node = self.begin
+        current_index = 0
+        prev_node = None
+
+        while current_node and current_node.value != obj:
+            prev_node = current_node
+            current_node = current_node.next
+            current_index += 1
+
+        if current_node is None:
+            return
+
+        if current_node is self.begin and current_node is self.end:
+            self.begin = self.end = None
+        elif current_node is self.begin:
+            self.begin = current_node.next
+        elif current_node is self.end:
+            self.end = prev_node
+            self.end.next = None
+        else:
+            prev_node.next = current_node.next
+
+        return current_index
 
     def first(self):
         """
         Return a *reference* to the first item, does not remove.
         """
-        pass
+        return self.begin and self.begin.value or None
 
     def last(self):
         """
         Return a reference to the last item, does not remove.
         """
-        pass
+        return self.end and self.end.value or None
 
     def count(self):
         """
         Counts the number of elements in the list.
         """
-        pass
+        count = 0
+        current_node = self.begin
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count
 
     def get(self, index):
         """
         Get the value at index.
         """
-        pass
+        current_index = 0
+        current_node = self.begin
+        while current_node and current_index != index:
+            current_node = current_node.next
+            current_index += 1
+        return current_node and current_node.value or None
 
     def dump(self, mark):
         """
         Debugging function that dumps the contents of the list.
         """
         pass
-
--- single_linked_list/test.py
+++ single_linked_list/test.py
@@ -1,15 +1,201 @@
-from unittest import TestCase
+import unittest
 
 from sgllist import *
 
 
-class TestSingleLinkedList(TestCase):
-
-    def test_create_single_linked_list_node(self):
-        pass
-
+RED = "red"
+BLUE = "blue"
+GREEN = "green"
 
 
-if __name__ == "__name__":
+class TestSingleLinkedList(unittest.TestCase):
+
+    def test_create_single_linked_list_node_without_next_node(self):
+        one = SingleLinkedListNode("one", None)
+        self.assertEqual(one.value, "one")
+        self.assertEqual(one.next, None)
+        self.assertEqual(repr(one), "[one->None]")
+
+    def test_create_single_linked_list_with_next_node(self):
+        two = SingleLinkedListNode("two", None)
+        one = SingleLinkedListNode("one", two)
+        self.assertEqual(one.value, "one")
+        self.assertEqual(two.value, "two")
+        self.assertEqual(one.next, two)
+        self.assertEqual(two.next, None)
+        self.assertEqual(repr(one), "[one->'two']")
+
+    def test_push(self):
+        colors = SingleLinkedList()
+        colors.push("Pthalo Blue")
+        self.assertEqual(colors.count(), 1)
+
+        colors.push("Ultramarine Blue")
+        self.assertEqual(colors.count(), 2)
+
+    def test_pop(self):
+        colors = SingleLinkedList()
+        magenta = "Magenta"
+        alizarin = "Alizarin"
+        colors.push(magenta)
+        colors.push(alizarin)
+        self.assertEqual(colors.pop(), alizarin)
+        self.assertEqual(colors.pop(), magenta)
+        self.assertEqual(colors.pop(), None)
+        self.assertEqual(colors.pop(), None)
+
+    def test_shift(self):
+        colors = SingleLinkedList()
+
+        colors.shift(BLUE)
+        self.assertEqual(colors.count(), 1)
+
+        colors.shift(GREEN)
+        self.assertEqual(colors.count(), 2)
+
+        self.assertEqual(colors.pop(), BLUE)
+        self.assertEqual(colors.count(), 1)
+
+        self.assertEqual(colors.pop(), GREEN)
+        self.assertEqual(colors.count(), 0)
+
+    def test_first(self):
+        colors = SingleLinkedList()
+
+        colors.push(BLUE)
+        self.assertEqual(colors.first(), BLUE)
+
+        colors.push(GREEN)
+        self.assertEqual(colors.first(), BLUE)
+
+        colors.shift(RED)
+        self.assertEqual(colors.first(), RED)
+
+    def test_last(self):
+        colors = SingleLinkedList()
+
+        colors.push(RED)
+        self.assertEqual(colors.last(), RED)
+
+        colors.push(BLUE)
+        self.assertEqual(colors.last(), BLUE)
+
+        colors.shift(GREEN)
+        self.assertEqual(colors.last(), BLUE)
+
+    def test_get(self):
+        colors = SingleLinkedList()
+        colors.push("Vermillion")
+        self.assertEqual(colors.get(0), "Vermillion")
+        colors.push("Sap Green")
+        self.assertEqual(colors.get(0), "Vermillion")
+        self.assertEqual(colors.get(1), "Sap Green")
+        colors.push("Cadmium Yellow Light")
+        self.assertEqual(colors.get(0), "Vermillion")
+        self.assertEqual(colors.get(1), "Sap Green")
+        self.assertEqual(colors.get(2), "Cadmium Yellow Light")
+        self.assertEqual(colors.pop(), "Cadmium Yellow Light")
+        self.assertEqual(colors.get(0), "Vermillion")
+        self.assertEqual(colors.get(1), "Sap Green")
+        self.assertEqual(colors.get(2), None)
+        colors.pop()
+        self.assertEqual(colors.get(0), "Vermillion")
+        colors.pop()
+        self.assertEqual(colors.get(0), None)
+
+
+class TestFromZedShaw(unittest.TestCase):
+
+    def test_push(self):
+        colors = SingleLinkedList()
+        colors.push("Pthalo Blue")
+        assert colors.count() == 1
+        colors.push("Ultramarine Blue")
+        assert colors.count() == 2
+
+    def test_pop(self):
+        colors = SingleLinkedList()
+        colors.push("Magenta")
+        colors.push("Alizarin")
+        assert colors.pop() == "Alizarin"
+        assert colors.pop() == "Magenta"
+        assert colors.pop() == None
+
+    def test_unshift(self):
+        colors = SingleLinkedList()
+        colors.push("Viridian")
+        colors.push("Sap Green")
+        colors.push("Van Dyke")
+        assert colors.unshift() == "Viridian"
+        assert colors.unshift() == "Sap Green"
+        assert colors.unshift() == "Van Dyke"
+        assert colors.unshift() == None
+
+    def test_shift(self):
+        colors = SingleLinkedList()
+        colors.shift("Cadmium Orange")
+        assert colors.count() == 1
+
+        colors.shift("Carbazole Violet")
+        assert colors.count() == 2
+
+        assert colors.pop() == "Cadmium Orange"
+        assert colors.count() == 1
+        assert colors.pop() == "Carbazole Violet"
+        assert colors.count() == 0
+
+    def test_remove(self):
+        colors = SingleLinkedList()
+        colors.push("Cobalt")
+        colors.push("Zinc White")
+        colors.push("Nickle Yellow")
+        colors.push("Perinone")
+        assert colors.remove("Cobalt") == 0
+        colors.dump("before perinone")
+        assert colors.remove("Perinone") == 2
+        colors.dump("after perinone")
+        assert colors.remove("Nickle Yellow") == 1
+        assert colors.remove("Zinc White") == 0
+
+    def test_first(self):
+        colors = SingleLinkedList()
+        colors.push("Cadmium Red Light")
+        assert colors.first() == "Cadmium Red Light"
+        colors.push("Hansa Yellow")
+        assert colors.first() == "Cadmium Red Light"
+        colors.shift("Pthalo Green")
+        assert colors.first() == "Pthalo Green"
+
+    def test_last(self):
+        colors = SingleLinkedList()
+        colors.push("Cadmium Red Light")
+        assert colors.last() == "Cadmium Red Light"
+        colors.push("Hansa Yellow")
+        assert colors.last() == "Hansa Yellow"
+        colors.shift("Pthalo Green")
+        assert colors.last() == "Hansa Yellow"
+
+    def test_get(self):
+        colors = SingleLinkedList()
+        colors.push("Vermillion")
+        assert colors.get(0) == "Vermillion"
+        colors.push("Sap Green")
+        assert colors.get(0) == "Vermillion"
+        assert colors.get(1) == "Sap Green"
+        colors.push("Cadmium Yellow Light")
+        assert colors.get(0) == "Vermillion"
+        assert colors.get(1) == "Sap Green"
+        assert colors.get(2) == "Cadmium Yellow Light"
+        assert colors.pop() == "Cadmium Yellow Light"
+        assert colors.get(0) == "Vermillion"
+        assert colors.get(1) == "Sap Green"
+        assert colors.get(2) == None
+        colors.pop()
+        assert colors.get(0) == "Vermillion"
+        colors.pop()
+        assert colors.get(0) == None
+
+
+if __name__ == "__main__":
     unittest.main()
 
